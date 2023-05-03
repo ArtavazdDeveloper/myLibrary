3bbc32ccd6de62d5a523c4ae69c79f8cac56fe56
ArtavazdDeveloper <120491711+ArtavazdDeveloper@users.noreply.github.com>
2023-05-03 14:35:28 +0400

    Delete mysql directory

--- mysql/company_employee/company.sql
+++
@@ -1,8 +0,0 @@
-create table company_employee.company
-(
-    id      INT(10) auto_increment
-        primary key,
-    name    VARCHAR(255) not null,
-    country VARCHAR(255) not null
-);
-
--- mysql/online_store/category.sql
+++
@@ -1,8 +0,0 @@
-create table online_store.category
-(
-    id          INT(10) auto_increment
-        primary key,
-    name        VARCHAR(255) not null,
-    description TEXT(65535)  not null
-);
-
